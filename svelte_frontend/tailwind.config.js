d69aa8a74a1e4a29132d44b4d803319d644db668
carl <carltayloruk@protonmail.com>
2025-06-30 16:03:46 +0100

    blah

---
+++ svelte_frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './index.html',
+        './src/**/*.{js,ts,jsx,tsx,svelte}',
+        './src/**/*.svelte'
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
