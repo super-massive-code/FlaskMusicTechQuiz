ef65a9db952bfdf21d43f7dd2752a537ab0df05b
super-massive-code <super-massive-code@users.noreply.github.com>
2025-06-30 14:05:10 +0000

    deploy: ebc5fcc208de8ac848c9716b0273e2ac95e69651

--- models.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class HighScore(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    score = db.Column(db.Integer, nullable=False)
